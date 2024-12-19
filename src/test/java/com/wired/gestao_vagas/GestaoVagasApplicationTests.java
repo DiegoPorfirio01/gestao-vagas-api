fc3ad0fcade240bb31e5ce45639a6856d8ff4922
Diego <porfiriodiego64@gmail.com>
2024-12-19 12:00:41 -0300

    feat(init)

---
+++ src/test/java/com/wired/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wired.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
